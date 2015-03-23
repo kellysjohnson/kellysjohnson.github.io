a3ab8dc3b990b8a351221e3e42458d228269fbd3
kellysjohnson <kmshimko@gmail.com>
2015-03-22 23:54:46 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-function trains() {
-  var trains = 2*2;
+var seven=7;
+
+function mathIsGreat(seven) {
+  var mathIsGreat = seven*2;
+  return mathIsGreat;
 }
 
-console.log(trains);
+console.log(mathIsGreat);
 
 
